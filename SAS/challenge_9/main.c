a7542f10ef017ecfafa0d8dc649ff9fe54dd9a78
DIRECT0R-ctrl <youcodestudent@proton.me>
2025-09-02 17:58:57 +0200

    Update main.c

--- SAS/challenge_9/main.c
+++ SAS/challenge_9/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main() 
+{
     float x1, y1, z1, x2, y2, z2, distance;
 
     printf("please enter cordinates of the firs points (x1, y1, z1) :\n");
