a45f49bd398737dea6c67bbf1131e3534bfa9f22
DIRECTOR-ctrl <youcodestudent@proton.me>
2025-09-02 18:21:57 +0200

    Volume d'une Sphère

---
+++ SAS/challenge_10/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double radius, Volume;
+
+	const double PI = 3.14;
+	printf("please enter the radius : \n");
+	scanf("%lf", &radius);
+
+	Volume = (4.0 / 3.0) * PI * pow(radius, 3);
+
+	printf("volume of the sphere : %lf\n", Volume);
+}
