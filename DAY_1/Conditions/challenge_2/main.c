4bb48d384dbaf36d9971fd3a8156a3f7d3897edc
DIRECTOR-ctrl <youcodestudent@proton.me>
2025-09-03 18:06:34 +0200

    Identification de Voyelles

---
+++ DAY_1/Conditions/challenge_2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("please enter the character : ");
+    scanf("%c", &ch);
+
+    switch(ch) {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+            printf("%c its a vowel.\n", ch);
+            break;
+        default:
+            printf("%c its not a vowel.\n", ch);
+    }
+
+    return 0;
+}   
