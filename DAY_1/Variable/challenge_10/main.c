034c19bc480d10bf55fc129b747263c10b2e943d
DIRECTOR-ctrl <youcodestudent@proton.me>
2025-09-04 20:07:17 +0200

    Volume d'une Sphère

--- DAY_1/Variable/challenge_10/main.c
+++ DAY_1/Variable/challenge_10/main.c
@@ -2,13 +2,13 @@
 #include <math.h>
 int main()
 {
-	double radius, Volume;
+        double radius, Volume;
 
-	const double PI = 3.14;
-	printf("please enter the radius : \n");
-	scanf("%lf", &radius);
+        const double PI = 3.14;
+        printf("please enter the radius : \n");
+        scanf("%lf", &radius);
 
-	Volume = (4.0 / 3.0) * PI * pow(radius, 3);
+        Volume = (4.0 / 3.0) * PI * pow(radius, 3);
 
-	printf("volume of the sphere : %lf\n", Volume);
+        printf("volume of the sphere : %lf\n", Volume);
 }
