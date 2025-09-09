b5f130d367a30e9b050a4dd3614759e0f741f89a
DIRECTOR-ctrl <youcodestudent@proton.me>
2025-09-09 10:34:02 +0200

    Inversion de Chaîne de Caractères

---
+++ DAY_4/Fonctions/challenge_7/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int my_strlen(char *str)
+{
+	int len = 0;
+
+	while(str[len] != 0)
+	{
+		len++;
+	}
+	return len;
+}
+void inverser_chaine(char str[])
+{
+	
+	int len = my_strlen(str);
+	
+	if (str[len - 1] == '\n')
+	{
+		len--;
+	}
+
+	int i = len - 1;
+	while( i >= 0)
+	{
+		write(1, &str[i], 1);
+		write(1, " ", 1);
+		i--;
+	}
+
+	write(1, "\n", 1);
+
+}
+
+int main()
+{
+	char str[100];
+
+	printf("please enter a string : ");
+	fgets(str, sizeof(str), stdin);
+
+	inverser_chaine(str);
+
+	return 0;
+
+
+}
