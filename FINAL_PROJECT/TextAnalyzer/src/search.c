c03aa47e36d0ca148ae15ac003b4f4aa78b37a2b
DIRECTOR-ctrl <youcodestudent@proton.me>
2025-09-18 15:41:06 +0200

    Implement exact and partial search functions

---
+++ FINAL_PROJECT/TextAnalyzer/src/search.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/AymanTextlib.h"
+
+// Search for an exact word, returns index or -1
+int searchExact(const char *word, char dictionary[][MAX_WORD_LEN], int wordCount)
+{
+    for (int i = 0; i < wordCount; i++)
+    {
+        if (strcmp(dictionary[i], word) == 0) return i;
+    }
+    return -1;
+}
+
+// Search for partial word, prints all matches
+void searchPartial(const char *substr, char dictionary[][MAX_WORD_LEN], int counts[], int wordCount)
+{
+    int found = 0;
+    printf("\nPartial matches for '%s':\n", substr);
+
+    for (int i = 0; i < wordCount; i++)
+    {
+        if (strstr(dictionary[i], substr))
+        {
+            printf("%-15s : %d\n", dictionary[i], counts[i]);
+            found = 1;
+        }
+    }
+
+    if (!found)
+        printf("No match found.\n");
+}
+
